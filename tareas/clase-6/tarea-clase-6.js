014f8306868a80f0ef976a44fb10d2a49e0e425d
Juan6s <francarrizo2001@gmail.com>
2022-02-24 17:13:41 -0300

    finalizado

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -10,79 +10,139 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 const $botonCargar = document.querySelector("#boton-cargar");
 const $botonReset = document.querySelector("#boton-reset");
 const $division = document.querySelector("#familiares");
+
+const botonCalcular = document.createElement("button");
+botonCalcular.textContent = "Calcular";
+botonCalcular.id ="boton-calcular;"
+
 let elementos = [];
 
 
 
 
-
-function crearElementos(numero){
+function crearElementos(numero) {
     const TEXTO = "Edad del integrante Nº:"
+    
+    
+
+    let elementos = [];
+
+    for (let i = 0; i < numero; i++) {
+        const input = document.createElement("input");
+        input.type = "Number";
+        const label = document.createElement("label");
+
+        label.textContent = `${TEXTO} ${i + 1}`;
+        
+
+        elementos[i] = document.createElement("div");
+        elementos[i].appendChild(label);
+        elementos[i].appendChild(input);
 
 
-    let elementos=[]
-        for(let i = 0; i < numero; i++){
-            const input = document.createElement("input");
-            const label = document.createElement("label");
-            
-            label.textContent = `${TEXTO} ${i+1}`;
-            input.type = "Number";
 
-            elementos[i] = document.createElement("div");     
-            elementos[i].appendChild(label);
-            elementos[i].appendChild(input);
-            
-
-            
-        }
+    }
+    
+    elementos.push(botonCalcular);
+    
     return elementos;
 }
 
-function agregarElementos(elementos, division){
-    botonCalcular 
+function agregarElementos(elementos, division) {
     
-    for(let i = 0; i < elementos.length; i++){
+
+    for (let i = 0; i < elementos.length; i++) {
 
         division.appendChild(elementos[i])
 
     }
-    
+
 
 }
 
-
-function resetElementos(elementos,division){
-    for(let i = 0; i<elementos.length;i++){
+function resetElementos(elementos, division) {
+    for (let i = 0; i < elementos.length; i++) {
         division.removeChild(elementos[i]);
     }
-    
-    
+
+
     return [];
 
 }
 
+function obtenerValorElementos(elementos){
+    let valores = [];
+    for (let i =0; i<elementos.length-1; i++){
+        let subelemento = elementos[i]
+        valores[i] = Number(subelemento.lastChild.value);
+    }
+    return valores;
+}
 
-$botonCargar.onclick = function(){
-    const inputFamiliares = Number(document.querySelector("#input-cantidad-familiares").value);
+function menorEdad(valores){
+    let minimo = valores[0]
+    for(let i=1;i<valores;i++){
+        if(valores[i]<minimo){
+            minimo = valores[i];
+        }
+
+    }
+    return minimo;
+}
+
+function mayorEdad(valores){
+    let maximo = valores[0]
+    for(let i=1;i<valores.length;i++){
+        if(valores[i]>maximo){
+            maximo = valores[i];
+        }
+
+    }
+    return maximo;
+}
+
+function promedioEdades(valores){
+    let sumaTotal=0;
+    for(let i = 0;i<valores.length;i++){
+        sumaTotal+=valores[i]
+
+    }
+    console.log(sumaTotal)
+    return (sumaTotal/valores.length).toFixed(2);
     
-    if (inputFamiliares <=0){
+}
+
+
+$botonCargar.onclick = function () {
+    const inputFamiliares = Number(document.querySelector("#input-cantidad-familiares").value);
+
+    if (inputFamiliares <= 0) {
         alert("Ingrese numeros mayores a 0!")
         return false;
     }
- 
 
-    if(elementos.length >1){
-        elementos = resetElementos(elementos,$division);
+
+    if (elementos.length > 1) {
+        elementos = resetElementos(elementos, $division);
     }
-    
+
     elementos = crearElementos(inputFamiliares);
-    agregarElementos(elementos,$division);
-    
-    
+    agregarElementos(elementos, $division);
+
 
 }
 
 
-$botonReset.onclick = function(){
-    elementos = resetElementos(elementos,$division);
+$botonReset.onclick = function () {
+    elementos = resetElementos(elementos, $division);
+    
+}
+
+
+
+botonCalcular.onclick = function(){
+
+    document.querySelector("#edad-mayor").textContent = `La edad del mayor es:${mayorEdad(obtenerValorElementos(elementos))}`; 
+    document.querySelector("#edad-menor").textContent = `La edad del menor es:${menorEdad(obtenerValorElementos(elementos))}`; 
+    document.querySelector("#edad-promedio").textContent = `El promedio de edad es:${promedioEdades(obtenerValorElementos(elementos))}`; 
 }
